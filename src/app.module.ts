263a8773971ff811725c3b93f3bbcc6e25fe725b
marcelo <marcelo@dotse.com.br>
2022-03-16 17:52:32 -0300

    Chore: config database postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+// app.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
